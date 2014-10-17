0eada3cda06357eec65aff0870eedbb0b42537dc
Ana Paula <Ana Paula@200.145.43.194>
2014-10-16 22:22:52 -0300

    criação da pagina preview

---
+++ preview.php
@@ -0,0 +1,156 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sincronização</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="preview.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                    <div class="col-lg-5">
+                        <div class="page-header">
+                            <h1>NSac</h1>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <td>Usuário</td>
+                                        <td>Subgrupo</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="2" class="principal">Alunos</td>                                
+                                    </tr>
+                                    <tr>
+                                        <td>1257020</td>
+                                        <td>73C</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1257016</td>
+                                        <td>73C</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" class="principal">Professores</td>
+
+                                    </tr>
+                                     <tr>
+                                        <td>Celso</td>
+                                        <td>&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vitor</td>
+                                        <td>&nbsp;</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div><!-- /tabela -->
+                    </div><!-- /coluna nsac -->
+
+                    <div class="col-lg-2">
+                        <div class="page-header">
+                            <h1>&nbsp;</h1>
+                        </div> 
+                        <div class="table-responsive ">
+                            <table class=" table acao">
+                                <thead>
+                                    <tr><td>&nbsp;</td></tr>
+                                </thead>
+                                <tbody>
+                                    <tr><td>Deletado</td></tr>
+                                    <tr><td>Deletado</td></tr>
+                                    <tr><td>&nbsp;</td></tr>
+                                    <tr><td>&nbsp;</td></tr>
+                                    <tr><td>&nbsp;</td></tr>
+                                    <tr><td>&nbsp;</td></tr>
+                                </tbody>
+                                
+
+                            </table>
+                        </div>
+                    </div><!-- /coluna ações -->
+
+                    <div class="col-lg-5">
+                        <div class="page-header">
+                            <h1>Samba</h1>
+                        </div>
+                                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <td>Usuário</td>
+                                        <td>Subgrupo</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="2" class="principal">Alunos</td>                                
+                                    </tr>
+                                    <tr>
+                                        <td>1257020</td>
+                                        <td>73C</td>
+                                    </tr>
+                                    <tr>
+                                        <td>1257016</td>
+                                        <td>73C</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2" class="principal">Professores</td>
+
+                                    </tr>
+                                     <tr>
+                                        <td>Celso</td>
+                                        <td>&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vitor</td>
+                                        <td>&nbsp;</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div><!-- /tabela -->
+                    </div><!-- /coluna samba -->
+            </div><!-- /row-->
+            
+            <div class="row botoes">
+                <div class="col-lg-12">
+                    <button type="button" class="btn btn-lg btn-danger"><span class="glyphicon glyphicon-remove"></span> Parar</button>
+                    <button type="button" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-lock"></span> Sincronizar senhas</button>
+                    <button type="button" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-refresh"></span> Sincronizar</button>
+                </div>
+            </div>
+            
+        </div><!-- /container -->
+        <div class="footer" style="overflow-y: auto">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit 
+                                    sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus 
+                                    magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. 
+                                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. 
+                                    Aenean lacinia bibendum nulla sed consectetur.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit 
+                                    sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus 
+                                    magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. 
+                                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. 
+                                    Aenean lacinia bibendum nulla sed consectetur.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit 
+                                    sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus 
+                                    magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. 
+                                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. 
+                                    Aenean lacinia bibendum nulla sed consectetur.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </body>
+    
+    <script type="text/javascript" src="jquery.js" ></script>
+    
+</html>
